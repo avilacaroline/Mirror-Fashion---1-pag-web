58ee46ab1f07db10e6d2f19fa3cd292f2d8f4b6e
Caroline Ávila <avila_caroline@live.com>
2021-02-03 19:37:14 -0300

    commit docs

---
+++ JS/produto.js
@@ -0,0 +1,8 @@
+    let inputTamanho = document.querySelector('[name=tamanho]')
+let outputTamanho = document.querySelector('[name=valortamanho]')
+
+function mostraTamanho () {
+        ou tputTamanho.value = inputTamanho.value
+}
+
+inputTamanho.oninput = mostraTamanho
